5a08565f970facd08c1bfea25da926728ec12cda
Aquiles Popovich <popovichaquiles@gmail.com>
2024-05-10 02:08:26 -0300

    footer y clientes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,57 +1,61 @@
-import styles from './footer.module.css'; 
-import logo from '../../assets/logoPrueba.png'
+import React from 'react';
+import styles from './footer.module.css';
+import logo from '../../assets/logoPrueba.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+
 
 const Footer = () => {
     return (
         <div className={styles.container}>
             <div className={styles.content}>
-                <div><img src={logo} alt="" /></div>
-                <div>
+                <div className={styles.DivcontentIMG}><img src={logo} alt="" /> <div></div> <div></div></div>
+                <div className={styles.Divcontent}>
                     <div>
                         <h3>Soluciones</h3>
                         <ul>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
+                            <li>CORE BANKING SOFTWARE SOLUTION</li>
+                            <li>LOAN MANAGEMENT SYSTEM</li>
+                            <li>REPORTING</li>
+                            <li>MIFOS</li>
+                            <li>FINERACT</li>
                         </ul>
                     </div>
                     <div>
                         <h3>Compania</h3>
                         <ul>
-                            <li></li>
-                            <li></li>
-                            <li></li>
+                            <li>OUR STORY</li>
+                            <li>CONTACT US</li>
+                            <li>FITER READ</li>
                         </ul>
                     </div>
                     <div>
                         <h3>Casos de Exito</h3>
                         <ul>
-                            <li></li>
-                            <li></li>
+                            <li>Casos de Exito</li>
                         </ul>
                     </div>
                     <div>
                         <h3>Conecta con Nosotros</h3>
-                        <div>
+                        <div className={styles.barraTop}>
                             <p>armelamorrortu9678@gmail.com</p>
-                            <div>
+                            <div className={styles.contentLinks}>
                                 <ul>
-                                    <li></li>
-                                    <li></li>
-                                    <li></li>
+                                    <li><a className={styles.link} href="https://www.linkedin.com/in/armeldeamorrortu/"><FontAwesomeIcon icon={faLinkedin} /></a></li>
+                                    <li><a className={styles.link} href="https://www.instagram.com/armeldeamorrortu/"><FontAwesomeIcon icon={faInstagram} /></a></li>
+                                    <li><a className={styles.link} href=""><FontAwesomeIcon icon={faWhatsapp} /></a></li>
                                 </ul>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div>
-                    <div></div>
-                    <div></div>
+                <div className={styles.Divcontent}>
+                    <div>© 2024. ALL RIGHTS RESERVED.</div>
+                    <div>TERMS AND CONDITIONS</div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Footer;
--- src/components/clientes/Clientes.jsx
+++ src/components/clientes/Clientes.jsx
@@ -10,38 +10,45 @@ const Clientes = () => {
     name: "hola",
     tiempo: "7 meses",
     Resultado: "Aumente las ventas en un 90%",
-    imgEmpresa: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBg59LNMeZr1VIsx1KJD0yROPeEzm5wWrU09cHhu1xbQ&s",
-    urlTestimonio: "" 
+    imgEmpresa: "https://www.liderlogo.es/wp-content/uploads/2022/12/pasted-image-0-1024x576.png",
+    urlTestimonio: "" ,
+    personaTestimonio: "Pepe(Ceo)"
       },{
     name: "bgp",
     tiempo: "2 meses",
     Resultado: "Aumente las ventas en un 90%",
     imgEmpresa: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBg59LNMeZr1VIsx1KJD0yROPeEzm5wWrU09cHhu1xbQ&s",
-    urlTestimonio: "" 
+    urlTestimonio: "" ,
+    personaTestimonio: "Armel(Ceo)"
       },{
     name: "mine",
     tiempo: "3 meses",
     Resultado: "Aumente las ventas en un 90%",
-    imgEmpresa: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBg59LNMeZr1VIsx1KJD0yROPeEzm5wWrU09cHhu1xbQ&s",
-    urlTestimonio: "" 
+    imgEmpresa: "https://w7.pngwing.com/pngs/493/809/png-transparent-kik-messenger-messaging-apps-mobile-app-kin-app-store-iphone-electronics-text-logo.png",
+    urlTestimonio: "" ,
+    personaTestimonio: "Man(Ceo)"
       },{
     name: "rara",
     tiempo: "1 mes",
     Resultado: "Aumente las ventas en un 90%",
-    imgEmpresa: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBg59LNMeZr1VIsx1KJD0yROPeEzm5wWrU09cHhu1xbQ&s",
-    urlTestimonio: "" 
+    imgEmpresa: "https://i.pinimg.com/550x/82/b2/1f/82b21fe6d9166c673eed585a5fc38ef5.jpg",
+    urlTestimonio: "" ,
+    personaTestimonio: "Juan(Ceo)"
       },{
     name: "lla",
     tiempo: "6 meses",
     Resultado: "Aumente las ventas en un 90%",
     imgEmpresa: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBg59LNMeZr1VIsx1KJD0yROPeEzm5wWrU09cHhu1xbQ&s",
-    urlTestimonio: "" 
+    urlTestimonio: "" ,
+    personaTestimonio: "Richi(Ceo)"
+
       },{
         name: "d",
         tiempo: "9 meses",
         Resultado: "Aumente las ventas en un 90%",
         imgEmpresa: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBg59LNMeZr1VIsx1KJD0yROPeEzm5wWrU09cHhu1xbQ&s",
-        urlTestimonio: "" 
+        urlTestimonio: "" ,
+        personaTestimonio: "Marta(Ceo)"
       }]
 
   return (
@@ -49,29 +56,34 @@ const Clientes = () => {
       <div className={`${style.cols} ${style.col0}`}>
         {seleccionado ? (
           <>
-          <div>
+          <div className={style.cardGrande}>
             <img src={seleccionado.imgEmpresa} alt="" />
-            <h2>{seleccionado.name}</h2>
-            <p>{seleccionado.tiempo}</p>
-            <p>{seleccionado.Resultado}</p>
-            <button>Ver testimonio</button>
+            <h2>Empresa: {seleccionado.name}</h2>
+            <p>Logros: {seleccionado.Resultado}</p>
+            <p>Tiempo: {seleccionado.tiempo}</p>
+            <button className={style.btnTestimonio}>Mira el testimonio de {seleccionado.personaTestimonio} </button>
             </div>
           </>
         ) : (
           <>
-          <div>
+          <div className={style.cardGrande}>
 
             <img src={clientesInfo[0].imgEmpresa} alt="" />
-            <h2>{clientesInfo[0].name}</h2>
-            <p>{clientesInfo[0].tiempo}</p>
-            <p>{clientesInfo[0].Resultado}</p>
-            <button>Ver testimonio</button>
+            <h2>Empresa: {clientesInfo[0].name}</h2>
+            <p>Logros: {clientesInfo[0].Resultado}</p>
+            <p>Tiempo: {clientesInfo[0].tiempo}</p>
+            <button className={style.btnTestimonio}>Mira el testimonio de {clientesInfo[0].personaTestimonio} </button>
           </div>
 
           </>
         )}
       </div>
       <div className={`${style.cols} ${style.col1}`}>
+        <div className={style.casosDeExitos}>
+        <h3>Casos de Exitos</h3>
+
+        </div>
+        <div className={style.cards}>
         {clientesInfo.map((cliente, index) => {
           return (
             <div key={index} className={style.card} onClick={() => setSeleccionado(cliente)}>
@@ -80,6 +92,8 @@ const Clientes = () => {
             </div>
           )
         })}
+
+        </div>
       </div>
     </div>
   )
